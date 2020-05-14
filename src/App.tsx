ef433d09c17af9c1bef3de02b3315d2047c3c996
Greg Fitzgerald <gregory.fitzgerald@gmail.com>
2020-05-14 10:00:08 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import React, { useEffect, useState } from 'react';
+import {
+  IonApp,
+  IonContent,
+  IonPage,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+} from '@ionic/react';
+import { FCM } from '@ionic-native/fcm';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -23,15 +29,32 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [hasPermission, setHasPermission] = useState<boolean | null>();
+  const [token, setToken] = useState<string>();
+
+  useEffect(() => {
+    FCM.hasPermission().then((permission) => setHasPermission(permission));
+    FCM.getToken().then((token) => setToken(token));
+  });
+  return (
+    <IonApp>
+      <IonPage>
+        <IonContent>
+          <IonList>
+            <IonItem>
+              <IonLabel>Permission: </IonLabel>
+              <IonInput value={String(hasPermission)} />
+            </IonItem>
+            <IonItem>
+              <IonLabel>Token: </IonLabel>
+              <IonInput value={token} />
+            </IonItem>
+          </IonList>
+        </IonContent>
+      </IonPage>
+    </IonApp>
+  );
+};
 
 export default App;
